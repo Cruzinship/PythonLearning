d3a87ca450c9dc763e66d88db484847a87a93d71
Gianpaulo_Cruz <cruzgianpaulo@gmail.com>
2023-07-07 15:40:58 -0400

    Funni Quiz game

--- .idea/39QuizGame.py
+++ .idea/39QuizGame.py
@@ -57,13 +57,27 @@ questions = {
     "Why do I code? ": "A",
     "When is my birthday? ": "B",
     "Who is my favorite super hero? " : "C",
-    "Is the Earth round? " : "A"
+    "The earth is round ": "A",
+    "What color is my shit? ": "D",
+    "Which of my birds is my favorite? ": "B",
+    "What's my favorite food? " :"D",
+    "What's my favorite drink? " :"B",
+    "What highscool did I graduate from? ": "C",
+    "Whats my Tennis/Sneaker shoe size? ": "A"
+
+
 }
 
 options = [["A. Money", "B. Birds" , "C. Always loved the idea of creating things myself", "D. Mark Zuckerburg"],
            ["A. July 27", "B. July 26", "C. July 16", "D. June 26"],
            ["A. Superman", "B. Batman", "C. Spiderman", "D. Walter White"],
-           ["A. True","B. False", "C. sometimes", "D. Whats an Earth?"]]
+           ["A. True","B. False", "C. sometimes", "D. Whats an Earth?"],
+           ["A. Red", "B. Black", "C. Yellow", "D. Brown"],
+           ["A. Green monster", "B. Kuku Nut", "C. Sparrow", "D. Teal monster"],
+           ["A. Lomo Saltado", "B. Ceviche", "C. Causa", "D. Lasangna"],
+           ["A. Water", "B. Maracuyá", "C. Chicha Morada", "D. Inca cola"],
+           ["A. John F Kennedy", "B. Rockville", "C. Richard Montgomery", "D. Walter Johnson"],
+           ["A. 13", "B. 12", "C. 12 1/2", "D. 11"]]
 
 new_game()
 
